2d920da0beb0e09b77fceda2110358348e8f4ff7
Bianca Oliveira <113205824+httpBianca@users.noreply.github.com>
2023-10-16 23:03:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,31 @@ const btn = document.querySelector('.top');
 const bttEnd = document.querySelector('.btt-end');
 const offers = document.querySelector('.offers-galery');
 
+/* ---- Funções ---- */
+
+/* ---- Muda a cor (adiciona uma classe ao header) ao alcançar um scroll de um certo tamanho ----- */
+
 function colorChange(){
-    if (document.documentElement.scrollTop > 300) {
+    if (document.documentElement.scrollTop > 50) {
         header.className = "color";
       } else {
         header.className = "";
       }
 }
 
+/* ---- Função pega a posição superior em px em relação ao pai e da um scroll até essa posição ----- */
+
 function backToTop(){
-  window.scrollTo(0,0);
+  const elementTop = header.offsetTop;
+  window.scrollTo({top: elementTop, behavior: 'smooth'});
 }
 
 function ToElement(){
   const elementTop = offers.offsetTop;
   window.scrollTo({top: elementTop, behavior: 'smooth'});
 }
+
+/* --- Chamada das funções ---- */
 
 window.addEventListener('scroll', function(){
     colorChange();
